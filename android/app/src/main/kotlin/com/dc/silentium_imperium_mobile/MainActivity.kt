75fb6a46d2088237ca7d5f317e9f65b8b63cb196
DanielChu <jiahao.chu.dc@gmail.com>
2021-04-03 12:35:03 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/dc/silentium_imperium_mobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dc.silentium_imperium_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+
+
+
+final String serverUri = "104.215.196.245";
+final int port = 8883;
+final String topicName = "Dart/Mqtt_client/testtopic";
